018d26b913ede72b5a93b65d795f23dca3a7d1ad
mvchernov <mvchernov@phoenixit.ru>
2022-08-05 17:53:43 +0300

    could not create DF

--- src/main/scala/functions/DStream.scala
+++ src/main/scala/functions/DStream.scala
@@ -1,24 +1,56 @@
 package functions
 
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql._
+import org.apache.spark.sql.types._
 import org.apache.spark.streaming._
 import org.apache.spark.streaming.kafka010._
 import org.apache.spark.streaming.kafka010.KafkaUtils.createRDD
 import org.apache.kafka.common.serialization.StringDeserializer
+import org.apache.spark.rdd.RDD
 import org.apache.spark.streaming.kafka010.LocationStrategies.PreferConsistent
 import org.apache.spark.streaming.kafka010.ConsumerStrategies.Subscribe
 import org.apache.spark.{SparkConf, SparkContext}
+//import java.util.List
+//import scala.collection.JavaConverters._
 import system._
 
-object DStream {
-  def readStream() : Unit = {
-    /*val spark = SparkSession.builder()
-      .master("local[*]")
-      .appName("SparkDStream")
-      .getOrCreate()*/
+object DStream extends Serializable {
 
-    val conf = new SparkConf().setAppName("SparkDStream").setMaster("local[*]")
-    val ssc = new StreamingContext(conf, Seconds(10))
+  def readStream() : Unit = {
+    val columns = Properties.schemaExpedia.fieldNames.toSeq
+
+    def threatLine(spark: SparkSession, str: String)  = {
+      import spark.implicits._
+      val columns = Properties.schemaExpedia.fieldNames
+      println("treating line... " + str)
+      /*val dfSeq = str.split(",")
+        .map(_.replace("[", ""))
+        .map(_.replace("]", ""))
+        .toSeq
+      val rdd = spark.sparkContext.parallelize(dfSeq)
+      val df = rdd.toDF()*/
+      val cols = Seq("language","users_count")
+      val data = Seq(("Java", "20000"), ("Python", "100000"), ("Scala", "3000"))
+      val rdd = spark.sparkContext.parallelize(data)
+      val dfFromRDD1 = rdd.toDF()
+      dfFromRDD1.printSchema()
+    }
+
+    def threatRDD(spark: SparkSession, rdd: RDD[String]): DataFrame = {
+      import spark.implicits._
+      val rddFlat = rdd.flatMap(l => l.split(","))
+      //val rddT = spark.sparkContext.parallelize(rddFlat.collect.toSeq.transpose)
+      val df = rddFlat.toDF()
+      df.show()
+      df
+    }
+
+    val spark = SparkSession.builder()
+      .appName("DFReader")
+      .master("local[*]")
+      .getOrCreate()
+    import spark.implicits._
+    val ssc = new StreamingContext(spark.sparkContext, Seconds(10))
 
     val kafkaParams = Map[String, Object](
       "bootstrap.servers" -> "localhost:9092",
@@ -33,23 +65,12 @@ object DStream {
       PreferConsistent,
       Subscribe[String, String](topic, kafkaParams))
 
-    stream.map(record => (println(record.key.length), println(record.value.length)))
     val lines = stream.map(_.value)
-    val words = lines.flatMap(_.split(" "))
-    val wordCounts = words.map(x => (x, 1L)).reduceByKey(_ + _)
-    wordCounts.print()
-    println(wordCounts.reduceByKey(_ + _))
+    lines.foreachRDD(_.foreach(println))
+    //lines.foreachRDD(rdd => threatRDD(spark, rdd))
+    lines.foreachRDD(_.foreach(v => threatLine(spark, v)))
 
-    ssc.start()             // Start the computation
-    ssc.awaitTermination()  // Wait for the computation to terminate
-    /*val offsetRanges = Array(
-      // topic, partition, inclusive starting offset, exclusive ending offset
-      OffsetRange("testtopic", 0, 0, 100),
-      OffsetRange("testtopic", 1, 0, 100)
-    )
-    val rdd = KafkaUtils.createRDD[String, String](sc, kafkaParams, offsetRanges, PreferConsistent)*/
-
-
+    ssc.start()
+    ssc.awaitTermination()
   }
-
 }
--- src/main/scala/system/Properties.scala
+++ src/main/scala/system/Properties.scala
@@ -1,5 +1,6 @@
 package system
 
+import org.apache.spark.sql.types._
 import java.util.Properties
 
 object Properties {
@@ -21,5 +22,29 @@ object Properties {
   propertiesKafka.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")
 
   val kafkaTopic = "testtopic"
+
+  val schemaExpedia: StructType = StructType(
+    StructField("id", LongType, nullable = false) ::
+      StructField("date_time", DateType, nullable = true) ::
+      StructField("site_name", IntegerType, nullable = true) ::
+      StructField("posa_continent", IntegerType, nullable = true) ::
+      StructField("user_location_country", IntegerType, nullable = true) ::
+      StructField("user_location_region", IntegerType, nullable = true) ::
+      StructField("user_location_city", IntegerType, nullable = true) ::
+      StructField("orig_destination_distance", DoubleType, nullable = true) ::
+      StructField("user_id", IntegerType, nullable = true) ::
+      StructField("is_mobile", IntegerType, nullable = true) ::
+      StructField("is_package", IntegerType, nullable = true) ::
+      StructField("channel", IntegerType, nullable = true) ::
+      StructField("srch_ci", DateType, nullable = true) ::
+      StructField("srch_co", DateType, nullable = true) ::
+      StructField("srch_adults_cnt", IntegerType, nullable = true) ::
+      StructField("srch_children_cnt", IntegerType, nullable = true) ::
+      StructField("srch_rm_cnt", IntegerType, nullable = true) ::
+      StructField("srch_destination_id", IntegerType, nullable = true) ::
+      StructField("srch_destination_type_id", IntegerType, nullable = true) ::
+      StructField("hotel_id", LongType, nullable = true) ::
+      Nil
+  )
 
 }
